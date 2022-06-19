fa15a942d6a0fc083e2b2f444fe538062548f651
abhishek-rajput119 <abrajput119@gmail.com>
2022-06-19 15:36:45 +0530

    home and submission templates added

--- mysite/online_judge/urls.py
+++ mysite/online_judge/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'online_judge'
 urlpatterns = [
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('home',views.home,name = 'home'),
+    path('submission',views.submission,name='submission')
 ]
--- mysite/online_judge/views.py
+++ mysite/online_judge/views.py
@@ -1,5 +1,14 @@
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def index(request):
     return HttpResponse("First Step.")
+
+
+def home(request):
+    return render(request,'pages/home.html')
+
+def submission(request):
+    return render(request,'pages/submission.html')
