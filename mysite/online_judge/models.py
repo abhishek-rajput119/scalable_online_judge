ef5f020c3b8aaf30c1030d1d93aaa7acc087aae0
abhishek-rajput119 <abrajput119@gmail.com>
2022-06-25 16:52:21 +0530

    created models for sqlite3

--- mysite/online_judge/models.py
+++ mysite/online_judge/models.py
@@ -1,28 +1,24 @@
+import datetime
+
 from django.db import models
 
 
 # Create your models here.
-class User(models.Model):
-    user_name = models.CharField
-    password = models.CharField
-    total_score = models.IntegerField
-
-
 class Problem(models.Model):
     problemId = models.IntegerField(primary_key=True)
-    description = models.CharField
-    difficulty = models.CharField
-    solved_status = models.BooleanField(default=False)
-    score = models.IntegerField
+    description = models.CharField(null=False, max_length=2000)
+    difficulty = models.CharField(null=False, max_length=10)
+    solved = models.BooleanField(default=False)
+    score = models.IntegerField(default=0)
 
 
-class TestCases(models.Model):
+class Testcase(models.Model):
     problemId = models.ForeignKey(Problem, on_delete=models.CASCADE)
-    input = models.CharField
-    output = models.CharField
+    input = models.CharField(null=False, max_length=1000)
+    output = models.CharField(null=False, max_length=1000)
 
 
 class Submission(models.Model):
     problemId = models.ForeignKey(Problem, on_delete=models.CASCADE)
-    time = models.TimeField
-    verdict = models.BooleanField
+    date = models.DateTimeField(default=datetime.timezone)
+    verdict = models.BooleanField()
